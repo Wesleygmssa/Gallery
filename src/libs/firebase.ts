027d4426db970a9ea55ea1d8416b01c0bf0a1d06
Guerra <wesley.moreira@vetta.digital>
2022-01-28 23:30:56 -0300

    Chore: Config fairebase

---
+++ src/libs/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRKYFRl_0f1a5HwHgsAmj1Acx8mPoGHpw",
+  authDomain: "d5reactgallery-68060.firebaseapp.com",
+  projectId: "d5reactgallery-68060",
+  storageBucket: "d5reactgallery-68060.appspot.com",
+  messagingSenderId: "361451390156",
+  appId: "1:361451390156:web:4e0075aac17612cc7c6e69",
+};
